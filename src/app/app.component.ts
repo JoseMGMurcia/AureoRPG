e565f9fb120b8b94d6ce3a84a260151a537f992c
JoseMGMurcia <guigatboise@gmail.com>
2021-08-25 02:23:46 +0200

    Starting config Added funcionality for multiple languajes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,27 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core'; 
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
+  styleUrls: ['app.component.scss']
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
+    { title: 'HOME', url: '/folder/HOME', icon: 'home' },
+    { title: 'CHARACTERS', url: '/folder/CHARACTERS', icon: 'people' },
+    { title: 'WIKI', url: '/folder/WIKI', icon: 'book' },
+    { title: 'DICE_SET', url: '/folder/DICE_SET', icon: 'dice' },
+    { title: 'INFO', url: '/folder/INFO', icon: 'information-circle' }
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
-  constructor() {}
+  public characters = ['Pj1', 'Pj2' ]
+  constructor(
+    private translate: TranslateService 
+  ) {
+    this.initializeApp();
+  }
+  
+  initializeApp() {
+    this.translate.setDefaultLang('es'); 
+  }
 }
