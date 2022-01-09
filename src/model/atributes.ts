b7ce1098f22c66ee8586c8a7af3abaa9d00e7781
JoseMGMurcia <guigatboise@gmail.com>
2022-01-10 00:09:36 +0100

    POJO Classes completed 2

--- src/model/atributes.ts
+++ src/model/atributes.ts
@@ -10,7 +10,7 @@ export class Atributes {
     private appearance: Atribute;
     private mind: Atribute;
     private sense: Atribute;
-
+ 
     constructor(){
         this.agility = new Atribute(0);
         this.comunication = new Atribute(0);
@@ -22,6 +22,80 @@ export class Atributes {
         this.mind = new Atribute(0);
         this.sense = new Atribute(0);
     }
+
+    //Getters & Setters
+
+    public getAgility(): Atribute {
+        return this.agility;
+    }
+
+    public setAgility(agility: Atribute): void {
+        this.agility = agility;
+    }
+
+    public getComunication(): Atribute {
+        return this.comunication;
+    }
+
+    public setComunication(comunication: Atribute): void {
+        this.comunication = comunication;
+    }
+
+    public getReflexes(): Atribute {
+        return this.reflexes;
+    }
+
+    public setReflexes(reflexes: Atribute): void {
+        this.reflexes = reflexes;
+    }
+
+    public getSoul(): Atribute {
+        return this.soul;
+    }
+
+    public setSoul(soul: Atribute): void {
+        this.soul = soul;
+    }
+
+    public getStrength(): Atribute {
+        return this.strength;
+    }
+
+    public setStrength(strength: Atribute): void {
+        this.strength = strength;
+    }
+
+    public getResistance(): Atribute {
+        return this.resistance;
+    }
+
+    public setResistance(resistance: Atribute): void {
+        this.resistance = resistance;
+    }
+
+    public getAppearance(): Atribute {
+        return this.appearance;
+    }
+
+    public setAppearance(appearance: Atribute): void {
+        this.appearance = appearance;
+    }
+
+    public getMind(): Atribute {
+        return this.mind;
+    }
+
+    public setMind(mind: Atribute): void {
+        this.mind = mind;
+    }
+
+    public getSense(): Atribute {
+        return this.sense;
+    }
+
+    public setSense(sense: Atribute): void {
+        this.sense = sense;
+    }
 }
 
 class Atribute {
@@ -30,5 +104,25 @@ class Atribute {
 
     constructor(valor: number){
         this.valor = valor;
+        this.mod = [];
     }
+
+    //Getter & Setter
+
+    public getValor(): number {
+        return this.valor;
+    }
+
+    public setValor(valor: number): void {
+        this.valor = valor;
+    }
+
+    public getMod(): number[] {
+        return this.mod;
+    }
+
+    public setMod(mod: number[]): void {
+        this.mod = mod;
+    }
+    
 }
